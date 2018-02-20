d8db99eea32fc44561c2628437bfc1e2c9323908
Brendo S <brendo_saldanha@hotmail.com>
2018-02-20 19:43:07 -0300

    Efetuando o Commit e o Push do DesafioFork

--- DesafioFork/ViewController.swift
+++ DesafioFork/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelMensagem: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.labelMensagem.text = self.retornaSaudacao()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func retornaSaudacao() -> String {
+        
+        // Resgatar a data atual
+        let agora = Date()
+        
+        // Criar datas de comparação
+        guard let meioDia = Calendar.current.date(bySettingHour: 12, minute: 00, second: 00, of: agora) else {return ""}
+        
+        guard let seisHoras = Calendar.current.date(bySettingHour: 18, minute: 00, second: 00, of: agora) else {return ""}
+        
+        // Comparar as datas e gerar as mensagens
+        if agora >= meioDia && agora < seisHoras {
+            
+            return "Boa tarde"
+            
+        } else if agora >= seisHoras {
+            
+            return "Boa noite"
+            
+        } else {
+            
+            return "Bom dia"
+            
+        }
+        
     }
-
-
+    
+    // MARK: Actions
+    
+    @IBAction func tocar(_ sender: UITapGestureRecognizer) {
+        
+        let alerta = UIAlertController(title: "", message: retornaSaudacao(), preferredStyle: .alert)
+        
+        let acaoOK = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alerta.addAction(acaoOK)
+        
+        present(alerta, animated: true, completion: nil)
+        
+    }
+    
 }
-
